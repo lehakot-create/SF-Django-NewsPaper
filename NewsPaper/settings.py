8ad90119a89348eef2b80abe15c13d8888e589b4
lehakot-create <lehakot85@inbox.ru>
2022-03-26 18:56:20 +0400

    Diploy

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -153,8 +153,8 @@ USE_TZ = False
 # )
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_ROOT]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
