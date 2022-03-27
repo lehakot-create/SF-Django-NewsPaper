c82a505f57972c7f4f29af8a56822c015bd618d6
lehakot-create <lehakot85@inbox.ru>
2022-03-27 09:37:16 +0400

    Diploy

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -147,9 +147,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # # STATIC_URL = '/static/'
