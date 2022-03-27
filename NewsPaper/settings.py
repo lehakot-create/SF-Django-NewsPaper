f82e5478c5d89a130ff2282e7e5e0996069e40e8
lehakot-create <lehakot85@inbox.ru>
2022-03-27 10:12:14 +0400

    Diploy

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -148,7 +148,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
