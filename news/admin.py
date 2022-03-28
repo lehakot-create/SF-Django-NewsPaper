7694b908b69b0b949dbfdb4e8d8c0056173a7f08
lehakot-create <lehakot85@inbox.ru>
2022-03-28 23:01:32 +0400

    add command runparser

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from .models import Post, Comment, Author, Category, PostCategory
+from modeltranslation.admin import TranslationAdmin
 
-admin.site.register(Post)
-admin.site.register(Comment)
+from .models import Post, Comment, Author, Category, PostCategory, CategorySubscribers
+
+
+class CategoryAdmin(TranslationAdmin):
+    model = Category
+
+
+class PostsAdmin(TranslationAdmin):
+    model = Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'choices', 'title', 'url')
+    search_fields = ('title', 'text')
+    ordering = ['-date_time']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'post')
+
+
+class CategorySubscriberAdmin(admin.ModelAdmin):
+    list_display = ('subscribers', 'category')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(PostCategory)
+admin.site.register(CategorySubscribers, CategorySubscriberAdmin)
