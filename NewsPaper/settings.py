fa42572608836be6a5e745062cdfa71415a97f3f
lehakot-create <lehakot85@inbox.ru>
2022-03-28 22:18:13 +0400

    add django_heroku

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -204,5 +205,7 @@ CELERY_TIMEZONE = "UTC"
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
